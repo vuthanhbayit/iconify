9629fd5369a4bc147545530c0078378714bee864
Vu Thanh Bay <vuthanhbayit@gmail.com>
2023-10-20 23:50:38 +0700

    feat: add plugin replace attribute for svgo

--- src/parse.ts
+++ src/parse.ts
@@ -1,7 +1,7 @@
 import type { IconifyJSON } from '@iconify/types'
 import { IconSet } from '@iconify/tools'
 import { optimize } from 'svgo'
-import { clearUndefined, map } from '@vt7/utils'
+import { forEach, isUndefined } from '@vt7/utils'
 
 export const parseIconifyJSON = (collection: IconifyJSON, transformAttrs: Record<string, any>) => {
   const iconSet = new IconSet(collection)
@@ -21,19 +21,25 @@ export const parseIconifyJSON = (collection: IconifyJSON, transformAttrs: Record
       const result = optimize(svg.toString(), {
         plugins: [
           {
-            name: 'removeAttrs',
-            params: {
-              attrs: Object.keys(transformAttrs),
-            },
-          },
-          {
-            name: 'addAttributesToSVGElement',
-            params: {
-              attributes: Object.values(
-                map(clearUndefined(transformAttrs), (value, key) => {
-                  return { [key]: value }
-                }),
-              ),
+            name: 'replaceAttribute',
+            fn: root => {
+              return {
+                element: {
+                  enter(node) {
+                    const attributes = node.attributes
+
+                    forEach(transformAttrs, (value, key) => {
+                      if (!attributes[key]) return
+
+                      if (isUndefined(value)) {
+                        delete node.attributes[key]
+                      } else {
+                        node.attributes[key] = value
+                      }
+                    })
+                  },
+                },
+              }
             },
           },
         ],
